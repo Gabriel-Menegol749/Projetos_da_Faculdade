e4a1b60dd985758e93b67e2ca464f381b2956f62
Gabriel Menegol <gsmenegol749@gmail.com>
2024-04-16 11:30:40 -0300

    Alterando layout dos botões de planejamento financeiro

---
+++ ProjetoFrontEnd/Javito.js
@@ -0,0 +1,34 @@
+/*
+Terei que adicionar estass funções nos botões mais adiante.
+
+
+<div class="conteudoFinanceiro">
+<h1 class="precoPlano"><span>R$29,90</span>/mês</h1>
+
+<div class="conteudoFinanceiro">
+<h1 class="precoPlano"><span>R$39,90</span>/mês</h1>
+
+<button class="planoEscolhido">ESCOLHA SEU PLANO</button>
+</div>
+
+<div class="conteudoFinanceiro">
+<h1 class="precoPlano"><span>R$55,90</span>/mês</h1>
+
+<button class="planoEscolhido">ESCOLHA SEU PLANO</button>
+
+*/
+
+/*Planos Financeiros*/
+const botaoFinanceiroMensal = document.getElementById('planosMensais');
+const botaoFinanceiroAnual = document.getElementById('planosAnuais');
+const botoesdosPlanos = document.querySelector('.botoesdosPlanos');
+
+botaoFinanceiroAnual.addEventListener('click', function() {
+
+});
+
+botaoFinanceiroMensal.addEventListener('click', function() {
+
+});
+
+
