504ab1b1a3f98d3f94579abb26f321c0356007fc
Gabriel Menegol <gsmenegol749@gmail.com>
2024-04-18 23:03:55 -0300

    estilizando a tela de login

--- ProjetoFrontEnd/TelaInicialJavaS.js
+++ ProjetoFrontEnd/TelaInicialJavaS.js
@@ -1,63 +1,63 @@
 
-/*Botões dos demais arquivos*/
-const botaoEntrar = document.getElementById("ENTRAR");
-const botaoRegistrar = document.getElementById("ASSINAR")
+    /*Botões dos demais arquivos*/
+    const botaoEntrar = document.getElementById("ENTRAR");
+    const botaoRegistrar = document.getElementById("ASSINAR")
 
-botaoEntrar.addEventListener("click", function(){
-    window.location.href ="TelaLoginHTML.html";
-});
-
-botaoRegistrar.addEventListener("click", function(){
-    window.location.href="TelaRegistroHTML.html";
-});
-/*Planos Financeiros*/
-const botaoFinanceiroMensal = document.getElementById('planosMensais');
-const botaoFinanceiroAnual = document.getElementById('planosAnuais');
-const botoesdosPlanos = document.querySelector('.botoesdosPlanos');
-const precoExplic = document.querySelectorAll(".precoExplic");
-const precoPlano1 = document.getElementById("precoPlano1");
-const precoPlano2 = document.getElementById("precoPlano2");
-const precoPlano3 = document.getElementById("precoPlano3");
-
-
-botaoFinanceiroAnual.addEventListener('click', function() {
-    botaoFinanceiroAnual.classList.add('botaoSelecionado');
-    botaoFinanceiroMensal.classList.remove('botaoSelecionado');
-
-    botaoFinanceiroAnual.style.backgroundColor = "white";
-    botaoFinanceiroAnual.style.color = "black";
-    precoExplic.forEach(element =>{
-        element.style.display = "block";
+    botaoEntrar.addEventListener("click", function(){
+        window.location.href ="TelaLoginHTML.html";
     });
-    botoesdosPlanos.classList.add("bordaAtivada");
 
-    botaoFinanceiroMensal.style.backgroundColor = "#00599d";
-    botaoFinanceiroMensal.style.color = "white"
-    precoPlano1.innerHTML = "<span>12x R$18,90</span>/mês"
-    precoPlano2.innerHTML = "<span>12x R$29,90</span>/mês"
-    precoPlano3.innerHTML = "<span>12x R$39,90</span>/mês"
-});
-
-botaoFinanceiroMensal.addEventListener('click', function() {
-    botaoFinanceiroMensal.classList.add('botaoSelecionado');
-    botaoFinanceiroAnual.classList.remove('botaoSelecionado');
-
-    botaoFinanceiroMensal.style.backgroundColor = "white";
-    botaoFinanceiroMensal.style.color = "black";
-    precoExplic.forEach(element =>{
-        element.style.display = "none";
+    botaoRegistrar.addEventListener("click", function(){
+        window.location.href="TelaRegistroHTML.html";
     });
-    botoesdosPlanos.classList.add("bordaAtivada");
+    /*Planos Financeiros*/
+    const botaoFinanceiroMensal = document.getElementById('planosMensais');
+    const botaoFinanceiroAnual = document.getElementById('planosAnuais');
+    const botoesdosPlanos = document.querySelector('.botoesdosPlanos');
+    const precoExplic = document.querySelectorAll(".precoExplic");
+    const precoPlano1 = document.getElementById("precoPlano1");
+    const precoPlano2 = document.getElementById("precoPlano2");
+    const precoPlano3 = document.getElementById("precoPlano3");
 
-    botaoFinanceiroAnual.style.backgroundColor = "#00599d";
-    botaoFinanceiroAnual.style.color = "white";
-    precoPlano1.innerHTML = "<span>R$29,90</span>/mês"
-    precoPlano2.innerHTML = "<span>R$39,90</span>/mês"
-    precoPlano3.innerHTML = "<span>R$55,90</span>/mês"
-});
 
-document.addEventListener('click', function(event){
-    if(!event.target.closest('.botoesdosPlanos')){
-        botoesdosPlanos.classList.remove("bordaAtivada");
-    }
-});
+    botaoFinanceiroAnual.addEventListener('click', function() {
+        botaoFinanceiroAnual.classList.add('botaoSelecionado');
+        botaoFinanceiroMensal.classList.remove('botaoSelecionado');
+
+        botaoFinanceiroAnual.style.backgroundColor = "white";
+        botaoFinanceiroAnual.style.color = "black";
+        precoExplic.forEach(element =>{
+            element.style.display = "block";
+        });
+        botoesdosPlanos.classList.add("bordaAtivada");
+
+        botaoFinanceiroMensal.style.backgroundColor = "#00599d";
+        botaoFinanceiroMensal.style.color = "white"
+        precoPlano1.innerHTML = "<span>12x R$18,90</span>/mês"
+        precoPlano2.innerHTML = "<span>12x R$29,90</span>/mês"
+        precoPlano3.innerHTML = "<span>12x R$39,90</span>/mês"
+    });
+
+    botaoFinanceiroMensal.addEventListener('click', function() {
+        botaoFinanceiroMensal.classList.add('botaoSelecionado');
+        botaoFinanceiroAnual.classList.remove('botaoSelecionado');
+
+        botaoFinanceiroMensal.style.backgroundColor = "white";
+        botaoFinanceiroMensal.style.color = "black";
+        precoExplic.forEach(element =>{
+            element.style.display = "none";
+        });
+        botoesdosPlanos.classList.add("bordaAtivada");
+
+        botaoFinanceiroAnual.style.backgroundColor = "#00599d";
+        botaoFinanceiroAnual.style.color = "white";
+        precoPlano1.innerHTML = "<span>R$29,90</span>/mês"
+        precoPlano2.innerHTML = "<span>R$39,90</span>/mês"
+        precoPlano3.innerHTML = "<span>R$55,90</span>/mês"
+    });
+
+    document.addEventListener('click', function(event){
+        if(!event.target.closest('.botoesdosPlanos')){
+            botoesdosPlanos.classList.remove("bordaAtivada");
+        }
+    });
---
+++ ProjetoFrontEnd/TelaLoginJavaS.js
@@ -0,0 +1,5 @@
+const botaoCadastro = document.getElementById("cadastro")
+
+botaoCadastro.addEventListener("click", function(){
+    window.location.href = "TelaRegistroHTML.html";
+});
