3d73090aa5f20e6d50b5d0bbb97733663b69a15e
Gabriel Menegol <gsmenegol749@gmail.com>
2024-04-19 22:08:40 -0300

    Finalizando estilização da tela de login

--- ProjetoFrontEnd/TelaLoginJavaS.js
+++ ProjetoFrontEnd/TelaLoginJavaS.js
@@ -1,5 +1,18 @@
-const botaoCadastro = document.getElementById("cadastro")
+const botaoCadastro = document.getElementById("cadastro");
+const inputSenha = document.getElementById("senha");
+const botaoRevelaSenha = document.getElementById("RevelaSenha");
+const iconeOlho = document.getElementById("olhoIcon");
 
 botaoCadastro.addEventListener("click", function(){
     window.location.href = "TelaRegistroHTML.html";
+});
+
+botaoRevelaSenha.addEventListener("click", function(){
+    if(inputSenha.type === 'password'){
+        inputSenha.type = 'text';
+        iconeOlho.src = "Sheets/eye-slash-solid.svg";
+    } else{
+        inputSenha.type = 'password';
+        iconeOlho.src ="Sheets/eye-solid.svg";
+    }
 });
