2b27cb84b49914c3ecb0b66d86069c28392c73da
Gabriel Menegol <gsmenegol749@gmail.com>
2024-04-21 12:35:31 -0300

    Finalizando a estilização da tela de planos, e adicionando funcionalidades JavaScript a ela

---
+++ ProjetoFrontEnd/TelaPlanosRegistroJavaS.js
@@ -0,0 +1,22 @@
+const planosDocumento = document.querySelectorAll(".Planos");
+const avisoNaoSelecionado = document.getElementById("aviso");
+const botaoContinuar = document.getElementById("continuar");
+
+let opcaoSelecionada = null;
+
+planosDocumento.forEach(plano => {
+    plano.addEventListener("click", () => {
+        planosDocumento.forEach(p => p.classList.remove("selecionado")); // Remover a classe "selecionado" de todas as opções
+        plano.classList.add("selecionado"); // Adicionar a classe "selecionado" apenas à opção clicada
+        opcaoSelecionada = plano;
+    });
+});
+
+
+botaoContinuar.addEventListener("click", () =>{
+    if(opcaoSelecionada === null){
+        avisoNaoSelecionado.style.display = "block";
+    } else{
+
+    }
+})
