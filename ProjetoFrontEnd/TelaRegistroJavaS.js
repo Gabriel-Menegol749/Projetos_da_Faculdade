be0ca74103987295d1921107df4a75d87b27eb6a
Gabriel Menegol <gsmenegol749@gmail.com>
2024-04-22 01:35:23 -0300

    Iniciando e adicionando os elementos da telaregistro, e adicionando os arquivos da tela principal do site

--- ProjetoFrontEnd/TelaRegistroJavaS.js
+++ ProjetoFrontEnd/TelaRegistroJavaS.js
@@ -4,3 +4,24 @@ botaoEntrar.addEventListener("click", function(){
     window.location.href = "TelaLoginHTML.html";
 })
 
+const inputSenha = document.getElementById("senha");
+const botaoRevelaSenha = document.getElementById("RevelaSenha");
+const iconeOlho = document.getElementById("olhoIcon");
+
+botaoRevelaSenha.addEventListener("click", function(){
+    if(inputSenha.type === 'password'){
+        inputSenha.type = 'text';
+        iconeOlho.src = "Sheets/eye-slash-solid.svg";
+    } else{
+        inputSenha.type = 'password';
+        iconeOlho.src ="Sheets/eye-solid.svg";
+    }
+});
+
+
+const botaoCriarConta = document.getElementById("CriarConta");
+
+botaoCriarConta.addEventListener("click", function(){
+    /*Adicionar verificação de inputs*/
+    window.location.href = "TelaPrincipalMaxHTML.html"
+});
