d4ca6eb880afe8cf4ea33334f20c9c1ad05c15db
Gabriel Menegol <gsmenegol749@gmail.com>
2024-04-23 09:20:24 -0300

    Corrigindo a responsividade das demais telas

--- ProjetoFrontEnd/TelaPrincipalMaxJavaS.js
+++ ProjetoFrontEnd/TelaPrincipalMaxJavaS.js
@@ -3,7 +3,9 @@ const botaoFilmeSerieSelecionado = document.getElementById("irParaConteudo");
     botaoFilmeSerieSelecionado.addEventListener("click", function(){
         window.location.href = "https://www.max.com/br/pt/shows/last-of-us/93ba22b1-833e-47ba-ae94-8ee7b9eefa9a";
     });
-
+function vaitopoSIte(){
+    window.scrollTo({top: 0, behavior: 'smooth'});
+}
 
 /*Banner Filmes Recomendados*/
 
